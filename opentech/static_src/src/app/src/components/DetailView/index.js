1de20b5a18acc60392b318e99f86b345d9e42922
Todd Dembrey <todd.dembrey@torchbox.com>
2019-01-21 13:59:54 +0000

    Addin react spring

--- opentech/static_src/src/app/src/components/DetailView/index.js
+++ opentech/static_src/src/app/src/components/DetailView/index.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { withWindowSizeListener } from 'react-window-size-listener';
+import { Transition, animated } from 'react-spring';
+
 
 import { clearCurrentSubmission } from '@actions/submissions';
 import DisplayPanel from '@containers/DisplayPanel';
-import SlideInRight from '@components/Transitions/SlideInRight'
-import SlideOutLeft from '@components/Transitions/SlideOutLeft'
+/* import SlideInRight from '@components/Transitions/SlideInRight'
+ * import SlideOutLeft from '@components/Transitions/SlideOutLeft' */
 import { getCurrentSubmissionID } from '@selectors/submissions';
 
 import './style.scss';
@@ -57,24 +59,19 @@ class DetailView extends Component {
         const { listing } = this.props;
 
         if (this.isMobile()) {
-            var activeDisplay;
-            if (this.state.listingShown){
-                activeDisplay = (
-                    <SlideOutLeft key={"listing"}>
-                        {listing}
-                    </SlideOutLeft>
-                )
-            } else {
-                activeDisplay = (
-                    <SlideInRight key={"display"}>
-                        { this.renderDisplay() }
-                    </SlideInRight>
-                )
-            }
-
             return (
                 <div className="detail-view">
-                    { activeDisplay }
+                    <Transition
+                        items={this.state.listingShown}
+                from={(item) => item ? { transform: 'translate3d(-100%,0,0)', transition: "transform  1000ms" } : { transform: 'translate3d(100%,0,0)', transition: "transform  1000ms" }}
+                        enter={{ transform: 'translate3d(0,0,0)', transition: "transform  1000ms"}}
+                        leave={(item) => item ? { transform: 'translate3d(-100%,0,0)', transition: "transform  1000ms"} : { transform: 'translate3d(100%,0,0)', transition: "transform  1000ms" }}>
+                        {toggle =>
+                            toggle
+                            ? props => <animated.div style={props}>{listing}</animated.div>
+                                    : props => <animated.div style={props}>{ this.renderDisplay() }</animated.div>
+                        }
+                    </Transition>
                 </div>
             )
         } else {
