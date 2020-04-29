b71d5d590576f51e61a93b10cb87545c9d20df52
Harum Helmy <harumendhah@gmail.com>
2020-04-29 14:20:07 -0400

    changed visibility option in the forms to multiple checkboxes from radio select, still need to figure out backend

--- hypha/apply/activity/forms.py
+++ hypha/apply/activity/forms.py
@@ -15,7 +15,7 @@ class CommentForm(forms.ModelForm):
             'message': 'Message',
         }
         widgets = {
-            'visibility': forms.RadioSelect(),
+            'visibility': forms.MultipleChoiceField(),
             'message': PagedownWidget(),
         }
 
@@ -23,19 +23,22 @@ class CommentForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.allowed_visibility = self._meta.model.visibility_for(user)
         self.visibility_choices = self._meta.model.visibility_choices_for(user)
+        self.fields['visibility'] = forms.MultipleChoiceField(choices=self.visibility_choices, widget=forms.CheckboxSelectMultiple)
         visibility = self.fields['visibility']
         # Set default visibility to "team" for staff and to "applicant" for everyone else.
         visibility.initial = self.visibility_choices[1] if user.is_apply_staff else self.visibility_choices[0]
-        if len(self.visibility_choices) > 1:
-            visibility.choices = self.visibility_choices
-            visibility.help_text = mark_safe('<br>'.join(
-                [VISIBILITY[choice] + ': ' + VISIBILILTY_HELP_TEXT[choice] for choice in self.allowed_visibility]
-            ))
-        else:
-            visibility.widget = forms.HiddenInput()
+        # visibility.choices = self.visibility_choices
+        # visibility.help_text = mark_safe('<br>'.join(
+        #     [VISIBILITY[choice] + ': ' + VISIBILILTY_HELP_TEXT[choice] for choice in self.allowed_visibility]
+        # ))
+
 
     def clean_visibility(self):
-        choice = self.cleaned_data['visibility']
+        data = self.cleaned_data['visibility']
+        choice = ' '.join(map(str, data)) 
+        print("self: ", self)
+        print(choice)
         if choice not in self.allowed_visibility:
+            print(choice)
             raise ValidationError('You do not have permission for that visibility.')
         return choice
